72b0314d9ad0453b919805141a95836f71fbff47
Anton Lioznov <lavton@gmail.com>
2025-07-24 14:14:32 +0800

    rewrite to svelte

---
+++ src/IgnoreNotice.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+  
+  let { whatIgnore }: {whatIgnore: Array<string>} = $props();
+	displayFilters = whatIgnore.slice(0, 10);
+
+	hasMore = whatIgnore.length > 10;
+</script>
+
+<div>
+	<p>Update ignore filters. Now they are:</p>
+	<ul>
+		{#each displayFilters as filter}
+			<li>{filter}</li>
+		{/each}
+
+		{#if hasMore}
+			<li>...</li>
+		{/if}
+	</ul>
+</div>
+
+<style>
+p, ul, li {
+	margin: 0;
+ }
+</style>
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 import { App, Notice, TFolder } from 'obsidian';
+import IgnoreNotice from './IgnoreNotice.svelte';
+import { mount } from 'svelte';
 
 export async function getAllDirs(app: App): Promise<Set<string>> {
 	const dirs = new Set<string>();
@@ -32,29 +34,19 @@ export function setIgnorence(app: App, whatIgnore: Array<String>) {
 }
 
 export function getIgnorenceNotice(whatIgnore: Array<string>): Notice {
-    // Create a DocumentFragment to build the notice content in memory
-    const fragment = document.createDocumentFragment();
+	const container = document.createElement('div');
 
-    // Add the introductory paragraph
-    fragment.createEl('p', { text: 'Update ignore filters. Now they are:' });
+	const component = mount (IgnoreNotice, {
+		target: container,
+		props: {
+			whatIgnore: whatIgnore
+		}
+	});
 
-    // Create the unordered list element
-    const ul = fragment.createEl('ul');
-
-    // Take only the first 10 items to display
-    const displayFilters = whatIgnore.slice(0, 10);
-
-    // Create an <li> for each item and append it to the <ul>
-    for (const filter of displayFilters) {
-        ul.createEl('li', { text: filter });
-    }
-
-    // If there were more than 10 items in the original array, add an ellipsis
-    if (whatIgnore.length > 10) {
-        ul.createEl('li', { text: '...' });
-    }
-
-    // Create the Notice using the completed DocumentFragment
-    // This will render the HTML correctly instead of showing the tags
-    return new Notice(fragment, 5000);
+	const fragment = document.createDocumentFragment();
+	while (container.firstChild) {
+		fragment.appendChild(container.firstChild);
+	}
+	return new Notice(fragment, 5000);
 }
+
